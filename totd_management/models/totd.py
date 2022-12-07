44b449c61634753c95aec653794dba7bbbe8c0f2
andg-odoo <andg@odoo.com>
2022-12-07 15:46:55 -0800

    missing s

--- totd_management/models/totd.py
+++ totd_management/models/totd.py
@@ -9,11 +9,12 @@ class TOTD(models.Model):
     assignee_monday = fields.Many2one(comodel_name='totd_management.people', string="Monday")
     assignee_tuesday = fields.Many2one(comodel_name='totd_management.people', string="Tuesday")
     assignee_wednesday = fields.Many2one(comodel_name='totd_management.people', string="Wednesday")
-    assignee_thursday = fields.Many2one(comodel_name='totd_management.people', tring="Thursday")
+    assignee_thursday = fields.Many2one(comodel_name='totd_management.people', string="Thursday")
     assignee_friday = fields.Many2one(comodel_name='totd_management.people', string="Friday")
 
 
 class People(models.Model):
-    _name = 'totd_management.people'
-    name = fields.Char(string="name")
+    _name = "totd_management.people"
+    _description = "ToTD Person"
+    name = fields.Char(string="Name")
     backup = fields.Boolean(default=False)
