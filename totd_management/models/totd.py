58d1a08ac76b2969236a26b0b16e674115e2813f
andg-odoo <andg@odoo.com>
2022-12-07 15:37:58 -0800

    Renamed module name

--- totd_management/models/totd.py
+++ totd_management/models/totd.py
@@ -2,18 +2,18 @@
 from odoo import models, fields, api
 
 class TOTD(models.Model):
-    _name = "totd.totd"
+    _name = "totd_management.totd"
     _description = "TOTD Model"
 
     week_num = fields.Integer(string="Week")
-    assignee_monday = fields.Many2one(comodel_name='totd.people', string="Monday")
-    assignee_tuesday = fields.Many2one(comodel_name='totd.people', string="Tuesday")
-    assignee_wednesday = fields.Many2one(comodel_name='totd.people', string="Wednesday")
-    assignee_thursday = fields.Many2one(comodel_name='totd.people', tring="Thursday")
-    assignee_friday = fields.Many2one(comodel_name='totd.people', string="Friday")
+    assignee_monday = fields.Many2one(comodel_name='totd_management.people', string="Monday")
+    assignee_tuesday = fields.Many2one(comodel_name='totd_management.people', string="Tuesday")
+    assignee_wednesday = fields.Many2one(comodel_name='totd_management.people', string="Wednesday")
+    assignee_thursday = fields.Many2one(comodel_name='totd_management.people', tring="Thursday")
+    assignee_friday = fields.Many2one(comodel_name='totd_management.people', string="Friday")
 
 
 class People(models.Model):
-    _name = 'totd.people'
+    _name = 'totd_management.people'
     name = fields.Char(string="name")
     backup = fields.Boolean(default=False)
